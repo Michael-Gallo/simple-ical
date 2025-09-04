cc42c42402804f10d1eef3d89233d685c406eaa2
Michael Gallo <michaelgallo@protonmail.com>
2025-09-03 21:45:50 -0400

    expand calendar model

---
+++ model/calendar.go
@@ -0,0 +1,30 @@
+package model
+
+// https://datatracker.ietf.org/doc/html/rfc5545#section-3.4
+type Calendar struct {
+	// Specifies the identifier corresponding to the
+	// highest version number or the minimum and maximum range of the
+	// iCalendar specification that is required in order to interpret the
+	// iCalendar object.
+	// https://datatracker.ietf.org/doc/html/rfc5545#section-3.7.4
+	Version string
+	// Product Identifier.
+	// This property specifies the identifier for the product that
+	// created the iCalendar object.
+	// https://datatracker.ietf.org/doc/html/rfc5545#section-3.7.3
+	ProdId string
+	// CalScale specifies the calendar scale used by the calendar component.
+	// https://datatracker.ietf.org/doc/html/rfc5545#section-3.7.1
+	CalScale string
+	// Method specifies the method used by the calendar component.
+	// https://datatracker.ietf.org/doc/html/rfc5545#section-3.7.2
+	Method    string
+	TimeZones []TimeZone
+
+	// a grouping of component properties that describe an event.
+	// https://datatracker.ietf.org/doc/html/rfc5545#section-3.6.1
+	Events []Event
+
+	// https://datatracker.ietf.org/doc/html/rfc5545#section-3.6.2
+	Todos []Todo
+}
---
+++ model/todo.go
@@ -0,0 +1,8 @@
+package model
+
+// a grouping of component properties that describe a to-do,
+// appointment, or journal entry.
+// https://datatracker.ietf.org/doc/html/rfc5545#section-3.6.2
+type Todo struct {
+	// TODO: fill out the Todo struct
+}
